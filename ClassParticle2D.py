2a484c5e2f6ff87546c733bd8dd15bb14e5fff70
xzhao287 <sanrorc@gmail.com>
2025-02-06 20:02:07 -0600

    Making particles static

--- ClassParticle2D.py
+++ ClassParticle2D.py
@@ -1,37 +1,39 @@
 import numpy as np
-from ClassSegline import Seg
+# from ClassSegline import Seg
 
 class Particle:
     def __init__(self,
                  position=np.zeros((2),dtype=float),
                  velocity=np.zeros((2),dtype=float),
-                 mass: float=1.0, name: str='H',  dt:float = 1, index:int=0) -> None:
+                 mass: float=1.0, name: str='H',  index:int=0, sigma:float = 1) -> None:
 
         self.pos = position
         self.vel = velocity
         self.m = mass
+        self.name = name
         self.n = index
-        self.pos_new = self.vel * dt
+        self.s = sigma # collision cross section for later particle interactions
 
-    def prop(self, dt):
-        '''propagation function, determine what'''
-        self.pos += dt*vel
+    # def prop(self):
+    #     '''propagation function, determine what'''
+    #     self.pos += self.dt*self.vel
+    #     return
 
-    def reflect(self, seg: Seg):
-        """
-        Reflect the particle off a segment
+    # def reflect(self, seg: Seg):
+    #     """
+    #     Reflect the particle off a segment
 
-        Args:
-            container_size (float): Size of the square container.
-        """
-        sega = seg.a
-        segb = seg.b
+    #     Args:
+    #         container_size (float): Size of the square container.
+    #     """
+    #     sega = seg.a
+    #     segb = seg.b
 
-        for i in range(2):  # Check for collisions in x and y directions
-            if self.position[i] < 0 or self.position[i] > container_size:
-                self.velocity[i] *= -1  # Reverse velocity component
+    #     for i in range(2):  # Check for collisions in x and y directions
+    #         if self.position[i] < 0 or self.position[i] > container_size:
+    #             self.velocity[i] *= -1  # Reverse velocity component
 
 if __name__ == '__main__':
     # print(np.zeros((2), dtype=float))
-    seg = Seg(start=np.array([1,1]), end=np.array([0,0]))
-    print(seg.a, seg.b)
+    # seg = Seg(start=np.array([1,1]), end=np.array([0,0]))
+    # print(seg.a, seg.b)
